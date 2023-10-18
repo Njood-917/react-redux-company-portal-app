8caf85e4153d18857a6f34515294d559a9c0cfa4
Njood-917 <njood9177@gmail.com>
2023-10-18 16:21:21 +0300

    redux company

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Button, Box, Grid, Typography } from '@mui/material'
 import { decrement, increment } from './features/counterSlice'
 import { RootState } from './store'
 import './App.css'
+import Company from './components/Company'
 
 function App() {
   const count = useSelector((state: RootState) => state.counter.value)
@@ -11,24 +12,9 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Vite + React + Toolkit + MUI</h1>
-      <Box sx={{ width: '100%' }}>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={5}>
-            <Button variant="contained" onClick={() => dispatch(increment())}>
-              Increment
-            </Button>
-          </Grid>
-          <Grid item xs={2}>
-            <Typography>{count}</Typography>
-          </Grid>
-          <Grid item xs={5}>
-            <Button variant="contained" onClick={() => dispatch(decrement())}>
-              Decrement
-            </Button>
-          </Grid>
-        </Grid>
-      </Box>
+      <h1>company</h1>
+      <Company/>
+   
     </div>
   )
 }
